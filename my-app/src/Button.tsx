6e65d98ff2156795d817e70230afda48dc46ab09
hogeno <masshi1108@gmail.com>
2023-01-01 22:25:28 +0900

    test tailwind

---
+++ my-app/src/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {
+    children?: React.ReactNode;
+};
+export const Button: React.FC<Props> = ({ children }) => {
+    return (
+            <button className="py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+                {children}
+            </button>
+            );
+};
