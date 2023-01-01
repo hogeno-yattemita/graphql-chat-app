dc7f82468782fe67e3608acc7ec07ec3bed04ed4
hogeno <masshi1108@gmail.com>
2023-01-01 22:44:01 +0900

    chat周りをコピー

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,28 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from './Button';
+//import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+//import { Button } from './Button';
+//
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.tsx</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//          <Button>Click!</Button>
+//      </header>
+//    </div>
+//  );
+//}
+//
+//export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <Button>Click!</Button>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Room } from './Room';
 
-export default App;
+const Input = styled.div`
+    padding: 4px;
+    margin: 0 0 4px;
+
+    input {
+        border: 1px solid #ccc;
+        padding: 2px;
+        font-size: 14px;
+    }
+`;
+
+export const App = () => {
+    const [name, setName] = useState('tester');
+    const [channel, setChannel] = useState('#gophers');
+
+    return (
+            <>
+            <Input>
+                name: <input value={name} onChange={(e) => setName(e.target.value)} />
+            </Input>
+            <Input>
+                channel: <input value={channel} onChange={(e) => setChannel(e.target.value)} />
+            </Input>
+            <Room channel={channel} name={name} />
+            </>
+            );
+
+};
+
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,19 +1,81 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import './index.css';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
+//
+//const root = ReactDOM.createRoot(
+//  document.getElementById('root') as HTMLElement
+//);
+//root.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>
+//);
+//
+//// If you want to start measuring performance in your app, pass a function
+//// to log results (for example: reportWebVitals(console.log))
+//// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import {
+    ApolloClient,
+    ApolloProvider,
+    HttpLink,
+    split,
+    InMemoryCache,
+} from '@apollo/client';
+import { WebSocketLink as ApolloWebSocketLink} from '@apollo/client/link/ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { App } from './App';
+import { WebSocketLink as GraphQLWSWebSocketLink } from './graphql-ws'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let wsLink;
+if (process.env.REACT_APP_WS_PROTOCOL === 'graphql-transport-ws') {
+    wsLink = new GraphQLWSWebSocketLink({
+        url: `ws://localhost:8085/query`
+    });
+} else {
+    wsLink = new ApolloWebSocketLink({
+        uri: `ws://localhost:8085/query`,
+        options: {
+            reconnect: true
+        }
+    });
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const httpLink = new HttpLink({ uri: 'http://localhost:8085/query' });
+
+
+// depending on what kind of operation is being sent
+const link = split(
+        // split based on operation type
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return kind === 'OperationDefinition' && operation === 'subscription';
+        },
+    wsLink,
+    httpLink,
+    );
+
+const apolloClient = new ApolloClient({
+    link: link,
+    cache: new InMemoryCache(),
+});
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default;
+        render(<NextApp/>);
+    })
+}
+
+function render(component) {
+    ReactDOM.render(<ApolloProvider client={apolloClient}>
+        {component}
+    </ApolloProvider>, document.getElementById('root'));
+}
+
+render(<App />);
